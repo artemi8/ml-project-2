53d3a3efb8118e31c35ab550fa70821fb9c67d49
Sudarsaan <sudarsaan@itglobe.com>
2023-12-13 11:38:43 +0000

    Tested Isolation Forest, and multiple variants for regression task

--- sudarsaan/utils/general_utils.py
+++ sudarsaan/utils/general_utils.py
@@ -23,12 +23,12 @@ def get_outlier_info(series):
     return lower_outlier_gate, upper_outlier_gate
 
 
-def get_outlier_val_counts(series, lower_outlier_gate, upper_outlier_gate):
+def get_outlier_val_counts(series, col_name, lower_outlier_gate, upper_outlier_gate):
     
     lower_outliers = series[series < lower_outlier_gate]
     upper_outliers = series[series > upper_outlier_gate]
 
-    return {lower_outlier_gate : [len(lower_outliers), lower_outliers], upper_outlier_gate : [len(upper_outliers), upper_outliers]}
+    return {"col_name" : col_name, lower_outlier_gate : [len(lower_outliers), lower_outliers], upper_outlier_gate : [len(upper_outliers), upper_outliers], "total_outliers" : len(lower_outliers)+len(upper_outliers)}
 
 def clean_outliers(df, col_name, lower_cutoff_thresh, upper_cutoff_thresh):
     
@@ -46,4 +46,11 @@ def train_val_test_split(Xy, train_ratio=0.7, only_test=False):
     test_count = total_count - (train_count + val_count)
     
     return Xy[:train_count], Xy[train_count:train_count+val_count], Xy[train_count+val_count:]
+
+
+def get_outlier_per_col(val_dict_list):
+    outlier_info = {}
+
+    for single in val_dict_list:
+        outlier_info[single['col_name']] = single['total_outliers']
     
